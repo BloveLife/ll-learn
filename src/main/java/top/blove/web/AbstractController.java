c7af3cfd59c0593db671d79f722f1a38e98bba12
yanlong <yanlong@hyphoneit.com>
2019-11-06 18:54:23 +0800

    feat: 框架搭建

--- src/main/java/top/blove/web/AbstractController.java
+++ src/main/java/top/blove/web/AbstractController.java
@@ -3,6 +3,7 @@ package top.blove.web;
 import lombok.Getter;
 import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.ModelAttribute;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -18,6 +19,12 @@ import javax.servlet.http.HttpServletResponse;
 public abstract class AbstractController implements CURDConstants {
     private HttpServletRequest request;
     private HttpServletResponse response;
+
+    @ModelAttribute
+    public void setRequestAndResponse(final HttpServletRequest request, final HttpServletResponse response) {
+        this.request = request;
+        this.response = response;
+    }
 
     @InitBinder
     public void initBinder(final WebDataBinder binder) {
